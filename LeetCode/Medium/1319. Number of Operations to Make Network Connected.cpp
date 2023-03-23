e81306df89c81b691c612f14831a26e4bb36a652
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2023-03-23 11:24:06 +0200

    Update 1319. Number of Operations to Make Network Connected.cpp

--- LeetCode/Medium/1319. Number of Operations to Make Network Connected.cpp
+++ LeetCode/Medium/1319. Number of Operations to Make Network Connected.cpp
@@ -5,19 +5,14 @@ public:
         return parent[x]==x ? x:parent[x]=find(parent[x]);
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int disc=0,extra=0;
-        iota(parent,parent+100001,0);
+        if(connections.size()<n-1)return -1;
+        iota(parent,parent+n,0);
         for(const auto& v : connections){
             int a = find(v[0]);
             int b = find(v[1]);
-            if(a==b)extra++;
-            parent[b]=a;
+            if(a==b)continue;
+            else parent[b]=a,n--;
         }
-        for(int i=0;i<n;i++)find(i);
-        unordered_set<int> s;
-        for(int i=0;i<n;i++)s.insert(parent[i]);
-        disc=s.size()-1;
-        if(disc>extra)return -1;
-        return disc;
+        return n-1;
     }
 };
